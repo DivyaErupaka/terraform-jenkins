1ac7b6c77eb3dae74ae8e81cc6fcda3062245c6a
DivyaErupaka <divyaerupaka1504@gmail.com>
2026-07-14 16:39:21 +0530

    Changed instance type to t3.micro

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "terraform_server" {
   ami                    = "ami-0c101f26f147fa7fd"
-  instance_type          = "t2.micro"
+  instance_type          = "t3.micro"
   subnet_id              = "subnet-054fdabde679510ca"
   vpc_security_group_ids = ["sg-0c41c8bd04c2741c2"]
 
