9d34ca71184da1eae96be251af2bd4bd13d77897
DivyaErupaka <divyaerupaka1504@gmail.com>
2026-07-14 16:44:29 +0530

    Changed instance type to Sg

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "terraform_server" {
   ami                    = "ami-0c101f26f147fa7fd"
   instance_type          = "t3.micro"
   subnet_id              = "subnet-054fdabde679510ca"
-  vpc_security_group_ids = ["sg-0c41c8bd04c2741c2"]
+  vpc_security_group_ids = ["sg-0309dd8bba1c5a8a2"]
 
   tags = {
     Name = "Terraform-Jenkins"
